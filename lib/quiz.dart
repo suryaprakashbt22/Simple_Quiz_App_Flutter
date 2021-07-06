316758290de9985159485525ada0e06e321ae02d
suryaprakashbt22 <56980864+suryaprakashbt22@users.noreply.github.com>
2021-07-07 03:26:03 +0530

    Update quiz.dart

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -20,34 +20,6 @@ class Quiz extends StatelessWidget {
         ...(questions[i]["Answerq"] as List<Map<String, Object>>).map((answer) {
           return Answer(() => answerQuestion(answer["score"]), answer["text"]);
         }).toList()
-        /*RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),*/
-        //Answer(_answerQuestion),
-        /*RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: _answerQuestion,
-              //onPressed: () => print('Answer 2 chosen!'),
-            ),*/
-        //Answer(_answerQuestion),
-        /*RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: _answerQuestion,
-              /*onPressed: () {
-                print('Answer 3chosen!');
-              },*/ // use of anonymous function
-            ),*/
-        //Answer(_answerQuestion),
-        // Text('Hi, I am Android, what is ur name?'),
-        //RaisedButton(
-        //child: Text("Better not tell 'em!"),
-        //onPressed: () => print("No can do!"),
-        //),
-        //RaisedButton(
-        //child: Text("Tell 'em!"),
-        //onPressed: () => print("I'm ur first app!"),
-        //),
       ],
     );
   }

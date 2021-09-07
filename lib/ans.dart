f5f5054fb97b04b967f170acc1ba3747d2d81e60
suryaprakashbt22 <56980864+suryaprakashbt22@users.noreply.github.com>
2021-09-07 14:22:40 +0530

    Update ans.dart

--- lib/ans.dart
+++ lib/ans.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
   final Function selectHandler;
   final String answerText;
+  int score;
   Answer(this.selectHandler, this.answerText);
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       child: RaisedButton(
-        color: Colors.teal[600],
-        textColor: Colors.white,
+        color: Colors.lightBlueAccent,
+        textColor: Colors.black,
         child: Text(answerText),
         onPressed: selectHandler,
       ),
